68e669c9ade4b01cb0d2deca2db71412d17a47cd
idipangkar-ai <idipangkar02@gmail.com>
2025-11-23 22:00:48 +0530

    QR req system get to post #3

--- server/src/modules/qr/qr.routes.js
+++ server/src/modules/qr/qr.routes.js
@@ -1,6 +1,4 @@
 // src/modules/qr/qr.routes.js
-// src/modules/qr/qr.routes.js
-
 
 import express from "express";
 import { scanQr } from "./qr.controller.js";
@@ -9,7 +7,7 @@ import { authMiddleware } from "../../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/generate", authMiddleware, generateQr);
+router.post("/generate", authMiddleware, generateQr);
 router.post("/scan", authMiddleware, scanQr);
 
 export default router;
